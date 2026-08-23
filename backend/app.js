0ad718b74386aaca025761b7c0098179d1b3cf4e
Aman Kumar Singh <singh123aman12345@gmail.com>
2026-08-23 11:56:39 +0530

    add dotenv and conntected to batabase

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,28 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
 
 const app = express();
 
+
+const uri = process.env.MONGODB_URI;
+const conntedDB = async()=>{
+    try {
+        await mongoose.connect(uri);
+        console.log("DB connected successfully")
+    } catch (error) {
+        console.log(error)
+    }
+}
+conntedDB();
 
 app.get('/',(req,res)=>{
     res.send("hello world")
 });
 
 
-app.listen(3000,()=>{
-    console.log("server is running...");
+app.listen(process.env.PORT,()=>{
+    console.log("server is running...",process.env.PORT);
 });
